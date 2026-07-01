3c6809d3f1d49fed3b48efcf7757e60bdc0e0b18
Aryan Jain <aryanjain6061@gmail.com>
2026-07-01 12:41:01 +0530

    chore: remove eks module, clean envs for ec2 kubeadm rebuild

--- terraform/modules/eks/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "cluster_name"     { value = aws_eks_cluster.main.name }
-output "cluster_endpoint" { value = aws_eks_cluster.main.endpoint }
-output "cluster_oidc_url" { value = aws_eks_cluster.main.identity[0].oidc[0].issuer }
